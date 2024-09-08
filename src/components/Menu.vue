b5fefb94bf8fbd302c504c49a0b8ae086b135982
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-09-08 18:21:00 -0400

    feat: implement a basic menu options

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,63 @@
 <template>
-  <div>
-    <h1>Menu</h1>
-    <p>Bem-vindo à página do Menu!</p>
-  </div>
+    <div id="menu-home">
+        <div class="container">
+          <div class="row">
+            <div id="login">
+              <div class="col-md-12">
+                <h1 class="text-center">Menu</h1>
+              </div>
+              <div class="col-md-12 text-center">
+                <button
+                    @click="start"
+                    id="start"
+                    type="button"
+                    class="btn btn-lg btn-dark"
+                >
+                  Start Game
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  methods: {
+    start() {
+      this.$router.push('start');
+    },
+  }
+};
 </script>
 
 <style scoped>
+#menu-home {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+}
+
 h1 {
-  font-size: 2rem;
-  color: #42b983;
+  font-size: 100px;
+  font-weight: bold;
+}
+
+button {
+  font-size: 40px;
+  padding: 20px 65px;
+}
+
+#login {
+  padding: 25px;
+  margin: 0 auto;
+  margin-top: 25%;
+}
+
+#start {
+  position: relative;
 }
 </style>
