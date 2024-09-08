ce83a53c76e50e00b2877a0701a1fa4d549da1fa
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-09-08 17:14:50 -0400

    feat: create default components snake game

---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+    <p>Bem-vindo à página do Menu!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #42b983;
+}
+</style>
